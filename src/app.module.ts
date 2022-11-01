7af31820f416e5c824660e362310d7dae9269601
MyungSeong <moves01@naver.com>
2022-11-01 11:18:29 +0900

    [UPDATE] TypeORM 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Joi from 'joi';
+
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmConfigAsync } from './boards/configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath:
+                process.env.NODE_ENV === 'production'
+                    ? ['.env.production.local', '.env.production']
+                    : process.env.NODE_ENV === 'development'
+                    ? ['.env.development.local', '.env.development']
+                    : ['.env.test.local', '.env.test'],
+            // ignoreEnvFile: process.env.NODE_ENV === 'production',
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string()
+                    .valid('development', 'production', 'test')
+                    .required(),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.string().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+            }),
+        }),
+        TypeOrmModule.forRootAsync(TypeOrmConfigAsync),
+        BoardsModule,
+    ],
 })
 export class AppModule {}
---
+++ src/boards/configs/typeorm.config.ts
@@ -0,0 +1,37 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+    TypeOrmModuleAsyncOptions,
+    TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { LoggerOptions } from 'typeorm';
+
+export class TypeOrmConfig {
+    static getTypeOrmConfig(
+        configService: ConfigService,
+    ): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: configService.get<string>('DB_HOST') || 'localhost',
+            port: +configService.get<number>('DB_PORT') || 3306,
+            username: configService.get<string>('DB_USERNAME'),
+            password: configService.get<string>('DB_PASSWORD'),
+            database: configService.get<string>('DB_NAME'),
+            entities: [
+                __dirname + '/../**/*.entity.{js,ts}',
+                __dirname + '/../**/!(*.test).{js,ts}',
+            ],
+            synchronize: true,
+            logging:
+                configService.get<LoggerOptions>('TYPEORM_LOGGING') || false,
+        };
+    }
+}
+
+export const TypeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    useFactory: async (
+        configService: ConfigService,
+    ): Promise<TypeOrmModuleOptions> =>
+        TypeOrmConfig.getTypeOrmConfig(configService),
+    inject: [ConfigService],
+};
